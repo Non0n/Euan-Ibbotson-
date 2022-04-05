c0199a225ee632108e2cfc9eb90141ef52b93a5e
Euan Ibbotson <97961368+Non0n@users.noreply.github.com>
2022-04-05 03:49:52 +0100

    Update plpoints.py

--- plpoints.py
+++ plpoints.py
@@ -2,14 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 #importing the needed libraries 
    
-Teams1 = []
-Points1 = []
+Teams1 = ['Man City', 'Liverpool', 'Chelsea', 'Tottenham', 'Arsenal', 'Man United', 'Wolves', 'Everton', 'Leicester', 'West Ham', 'Watford', 'Crystal Palace', 'Newcastle', 'Bournemouth', 'Burnley', 'Southampton', 'Brighton', 'Cardiff City', 'Fulham', 'Huddersfield']
+Points1 = [98, 97, 72, 71, 70, 66, 57, 54, 52, 52, 50, 49, 45, 45, 40, 39, 36, 34, 26, 16]
 
 Teams2 = ['Liverpool', 'Man City', 'Man United', 'Chelsea', 'Leicester', 'Tottenham', 'Wolves', 'Arsenal', 'Sheffield', 'Burnley', 'Southampton', 'Everton', 'Newcastle', 'Crystal Palace', 'Brighton', 'West Ham', 'Aston Villa', 'Bournemouth', 'Watford', 'Norwich City']
 Points2 = [99, 81, 66, 66, 62, 59, 59, 56, 54, 54, 52, 49, 44, 43, 41, 39, 35, 34, 34, 21]
 
-Teams3 = []
-Points3 = []
+Teams3 = ['Man City', 'Man United', 'Liverpool', 'Chelsea', 'Leicester', 'West Ham', 'Tottenham', 'Arsenal', 'Leeds', 'Everton', 'Aston Villa', 'Newcastle', 'Wolves', 'Crystal Palace', 'Southampton', 'Brighton', 'Burnley', 'Fulham', 'West Brom', 'Sheffield']
+Points3 = [86, 74, 69, 67, 66, 65, 62, 61, 59, 59, 55, 45, 45, 44, 43, 41, 39, 28, 26, 23]
 #Declaring all of the teams and the points that they recieved in that season
 
 def menu():
@@ -17,6 +17,7 @@ def menu():
     print("[1] 2018/19")
     print("[2] 2019/20")
     print("[3] 2020/21")
+    # Text based menu that allows the user to choose what season they would like to view
 
 
 menu()
@@ -29,7 +30,7 @@ while option1 != 0:
         print("[1] Bar Chart")
         print("[2] Pie Chart")
         print("[0] Exit the program.")
-        # Text based menu thats allows the user to input an option
+        # Text based menu thats allows the user to choose what type of visualisation they would like to view
       
         
         option2 = int(input("Please enter your option: "))
@@ -39,12 +40,14 @@ while option1 != 0:
             # This option will display a visualisation of a bar chart
                 plt.bar(Teams1, Points1)
                 plt.title('Premier League Table 2019/20 Season')
-                plt.xlabel('Teams1')
-                plt.ylabel('Points1')
-                plt.show()
+                plt.xlabel('Teams')
+                plt.ylabel('Points')
+                plt.show()  
+                plt.show(block=False)
+                plt.close('all')                           
                
             elif option2 == 2:
-            # his option will display a visualisation of a pie chart which displays the distribution of all points in the premier league table
+            # This option will display a visualisation of a pie chart which displays the distribution of all points added up in the premier league table
                 fig1 = plt.figure()
                 ax1 = fig1.add_axes([0,0,1,1])
                 ax1.axis('equal')
@@ -53,8 +56,9 @@ while option1 != 0:
             else:
                 print("Invalid option, please try again.")
 
-        print()        
+        print()    
         option2 = int(input("Please enter your option: "))
+        plt.close(all)
 
     elif option1 == 2:
         
@@ -73,8 +77,8 @@ while option1 != 0:
             # This option will display a visualisation of a bar chart
                 plt.bar(Teams2, Points2)
                 plt.title('Premier League Table 2019/20 Season')
-                plt.xlabel('Teams2')
-                plt.ylabel('Points2')
+                plt.xlabel('Teams')
+                plt.ylabel('Points')
                 plt.show()
                
             elif option3 == 2:
@@ -106,12 +110,12 @@ while option1 != 0:
             # This option will display a visualisation of a bar chart
                 plt.bar(Teams3, Points3)
                 plt.title('Premier League Table 2019/20 Season')
-                plt.xlabel('Teams3')
-                plt.ylabel('Points3')
+                plt.xlabel('Teams')
+                plt.ylabel('Points')
                 plt.show()
                
             elif option4 == 2:
-            # his option will display a visualisation of a pie chart which displays the distribution of all points in the premier league table
+            # This option will display a visualisation of a pie chart which displays the distribution of all points in the premier league table
                 fig3 = plt.figure()
                 ax3 = fig3.add_axes([0,0,1,1])
                 ax3.axis('equal')
@@ -125,6 +129,7 @@ while option1 != 0:
 
     else:
         print("Invalid option, please try again.")
+        # While loop if none of the requested input is given
 
     print()
     menu()
@@ -134,3 +139,5 @@ while option1 != 0:
 
 
 print("Thank you, goodbye.")
+plt.close('all')
+# Closes any remaining figures open
